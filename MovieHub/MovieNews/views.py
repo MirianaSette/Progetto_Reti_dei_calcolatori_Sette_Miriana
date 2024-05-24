447d61b5b0da2d75b06b5318e7d11c88d082ccc3
Miriana Sette <119594595+MirianaSette@users.noreply.github.com>
2024-05-24 22:06:20 +0200

    gestione canali

--- MovieHub/MovieNews/views.py
+++ MovieHub/MovieNews/views.py
@@ -1,6 +1,5 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
 
 from .models import User, RssFeed
 
@@ -22,8 +21,6 @@ def movielog(request):
         if user:
             # confronto la password catturata con quella del DB
             if user.password == password:
-                # lo porto alla home loggata, caricando gli RSS Feed
-                rss_feed = RssFeed.objects.all()
                 request.session["username"] = user.user
                 return redirect("home")
         # ricarico la pagina con un messaggio di errore
@@ -53,34 +50,45 @@ def movieregister(request):
         request.session["username"] = user.user
         return redirect("home")
 
-
     return render(request, 'MovieNews/registrazione.html')
 
 
 
 def moviehome(request):
     user_log = None
-    sub_message = "Prima effettua l'accesso."
+    url = None
+    sub_url = None
 
     rss_feed = RssFeed.objects.all()
 
     if "username" in request.session:
         user_log = request.session["username"]
+        user = User.objects.get(user = user_log)
 
         if request.method == "POST":
             url = request.POST.get("url_rss")
             channel = RssFeed.objects.get(url = url)
-            user = User.objects.filter(user = user_log).first()
-            if not user.rss_list.filter(url = url).exists():
+            if not channel in user.rss_list.all():
                 user.rss_list.add(channel)
-                sub_message = "Canale aggiunto"
             else:
-                sub_message = "Canale gia' presente"
+                user.rss_list.remove(channel)
+
+            user.save()
+        sub_url = list(user.rss_list.all())
 
     return render(request, 'MovieNews/homePage.html',
-                  {'user': user_log, 'rss': rss_feed, 'mex': sub_message})
+                  {'user': user_log, 'rss': rss_feed, 'sub_url': sub_url})
 
 
 
 def moviepersonal(request):
-    return render(request, 'MovieNews/personalNews.html')
+    news = None
+
+    if "username" not in request.session:
+        return HttpResponseNotFound("Non sei autenticato per visionare contenuti privati")
+    else:
+        username = request.session["username"]
+        user = User.objects.get(user = username)
+
+        return render(request, 'MovieNews/personalNews.html',
+                      {'news': news, 'username': username})
